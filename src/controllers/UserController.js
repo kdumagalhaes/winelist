858f19242ac0a9fa1995226d308bbbdece36b7e9
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-08-30 18:19:40 -0300

    finished enviroment setup

---
+++ src/controllers/UserController.js
@@ -0,0 +1,30 @@
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+    async store(req, res){
+        try {
+            const {firstName, lastName, password, email} = req.body
+
+            const existentUser = await User.findOne({email})
+
+            if(!existentUser) {
+                const hashedPassword = await bcrypt.hash(password, 10)
+                const user = await User.create({
+                    firstName,
+                    lastName,
+                    password: hashedPassword,
+                    email,
+                })
+                return res.json(user)
+            }
+
+            return res.status(400).json({
+                message: "Email/User already exist. Do you want to login instead?"
+            })
+
+        } catch (error) {
+            throw Error(`Error while registering a new user: ${error}`)
+        }
+    }
+}
